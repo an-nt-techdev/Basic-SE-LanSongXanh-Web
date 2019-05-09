1433b49582ce3dae4d2bdd815b8da4d9d1bb8574
REIDEEN131199 <reideen131199@gmail.com>
2019-05-09 21:25:03 +0700

    Add View

---
+++ View/js/script.js
@@ -0,0 +1,14 @@
+
+// Event vote song
+var star = $('.fa-star');
+var vote = $('.fas.fa-star').length;
+
+$('.far').hover(function(){
+	
+	$(this).removeClass('far');
+	$(this).addClass('fas');
+	
+}, function(){
+	$(this).removeClass('fas');
+	$(this).addClass('far');
+});
---
+++ View/layout/head.php
@@ -0,0 +1,9 @@
+<meta charset="UTF-8">
+
+<title>Music | Feel with Eagle</title>
+
+<!-- Bootstrap CSS -->
+<link href="resource/libs/bootstrap-4.3.1-dist/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+<!-- Custom CSS -->
+<link href="resource/css/style.css" rel="stylesheet" type="text/css">

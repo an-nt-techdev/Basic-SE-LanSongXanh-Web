35706f495deacd1ee76fc1ca7ec0be03b33987c9
Thiên Ân <ndsg1964@gmail.com>
2019-05-19 13:22:53 +0700

    fix
    ...

--- Services/ArtistService.php
+++ Services/ArtistService.php
@@ -52,7 +52,7 @@ require_once SITE_ROOT."/Dao/SingerDao.php";
             return $this->singerDao->getSingerById($id);
         }
 
-        public function getSinger($nameSinger)
+        public function getSingerByName($nameSinger)
         {
             return $this->singerDao->getSingerByName($nameSinger);
         }

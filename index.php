81bd2dd72e6e628957bf7cf1c322af01cf7977dc
REIDEEN131199 <reideen131199@gmail.com>
2019-05-21 13:45:31 +0700

    repair

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ $homeUrl = $_SERVER['REQUEST_URI'];
 
 session_start();
 
-<<<<<<< HEAD
 // Sign in
 if (isset($_POST['user']))
 {
@@ -80,52 +79,4 @@ else
 				loadHome:	
 				require_once SITE_ROOT."/Controller/homeController.php";
 			}
-=======
-
-// if have GET 'search'
-if (isset($_GET['search'])) 
-{
-	require_once SITE_ROOT."/Controller/searchController.php";
-}
-else if (isset($_GET['v'])) 
-{
-	require_once SITE_ROOT."/Controller/watchController.php";
-}
-else if (isset($_GET['page'])) {
-	$page = $_GET['page'];
-	switch ($page) {
-		case 'ad': 
-			require_once SITE_ROOT."/Controller/dashboardController.php";
-			break;
-			
-		case 'infor':
-			require_once SITE_ROOT."/Controller/inforController.php";
-			break;
-
-		case 'rank': 
-			require_once SITE_ROOT."/Controller/rankController.php";
-			break;
-
-		case 'contact': 
-			require_once SITE_ROOT."/Controller/contactController.php";
-			break;
-
-		case 'singer': 
-			require_once SITE_ROOT."/Controller/artistController.php";
-			break;
-
-		case 'composer': 
-			require_once SITE_ROOT."/Controller/artistController.php";
-			break;
-
-		default: goto loadHome;
-	}
-}
-else 
-{
-	loadHome:	
-	require_once SITE_ROOT."/Controller/homeController.php";
-}
->>>>>>> 0ddb1da7dc4c509556721cc83c2ef003e7e8596a
-
 ?> 
